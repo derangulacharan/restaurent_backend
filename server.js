aad1f4424294293de5cbab6d556868f10a8c8f2b
DERANGULA CHARAN KUMAR <104844213+derangulacharan@users.noreply.github.com>
2023-11-28 15:28:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-var cors = require("cors");
-const cors = require("cors")
+const cors = require("cors"); // Only declare cors once
 const PORT = process.env.PORT || 8000;
 
-//Connect to MongoDB
+// Connect to MongoDB
 const dbURI = process.env.MONGODB_URI || "mongodb+srv://mohammadmujahiddin01:mujju123@database.ui1abg3.mongodb.net/database";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => console.log("Connected to database"))
+  .then((result) => console.log("Connected to the database"))
   .catch((err) => {
-    console.error("Error connecting to database:", err);
+    console.error("Error connecting to the database:", err);
   });
-
 
 // CORS Config
 app.use(cors());
 
-//Routes
-app.use("/user", require("./routes/user"))
-app.use("/reservation", require("./routes/reservation"))
+// Routes
+app.use("/user", require("./routes/user"));
+app.use("/reservation", require("./routes/reservation"));
 app.use('/restaurants', require('./routes/restaurant'));
-app.listen(PORT, () =>{
-    console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 app.use((req, res, next) => {
-    res.status(404).json({ error: 'Not Found' });
-  });
-  app.get("/",(req,res)=> {
-    res.setHeader("Access-Control-Alolow-Credentials","true")
-    res.send("API is running..");
-  });
-  
+  res.status(404).json({ error: 'Not Found' });
+});
+
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.send("API is running..");
+});
